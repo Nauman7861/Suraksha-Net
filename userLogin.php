c71126ef9bffab749c16ba2f733e0c1f081d2577
Nauman Patel <naumanpatel13@gmail.com>
2025-02-03 21:59:31 +0530

    Update userLogin.php

--- userLogin.php
+++ userLogin.php
@@ -6,7 +6,7 @@ session_start();
 <head>
 	<title>User Login</title>
 
-<!-- Materialize CSS -->
+<!-- Materialize CSS --> 
 	<link rel="stylesheet" type="text/css" href="css/materialize/materialize.min.css"> 
 <!-- Font Awesome -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
